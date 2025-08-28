91d6c8e5f9639683dacc71947cb27dc1eae61544
pablo96molina <54729876+pablo96molina@users.noreply.github.com>
2025-08-28 10:04:17 -0300

    Remove manual head element to fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
       <body className="font-body antialiased">
         {children}
         <Toaster />
